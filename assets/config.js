d6e28df10530614df000c75802e434a08f249b94
pon4ic984 <romanceuy1@gmail.com>
2026-06-12 14:15:53 +0300

    Delete assets directory

--- assets/config.js
+++
@@ -1,8 +0,0 @@
-window.FRAMELOCK_CONFIG = {
-  projectName: 'FrameLock',
-  serverName: 'Serial Studio',
-  // Для GitHub Pages оставь пустым. Если позже будет backend, укажи URL:
-  // submissionEndpoint: 'https://your-backend.example.com/api/applications'
-  submissionEndpoint: '',
-  // Не кладите сюда Velocity API token. В статическом сайте его украдут.
-};
